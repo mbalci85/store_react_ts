464b197e2bedc0d169b815a4795addad4f8b543b
mustafa balci <mbalci85@gmail.com>
2023-10-12 23:03:55 +0100

    lazy loading added

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,87 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
 import './App.css';
-import { Box } from '@mui/material';
-import Header from './components/Header/Header';
-import HomePage from './pages/HomePage/HomePage';
-import About from './pages/About/About';
-import ProductDetail from './pages/ProductDetail/ProductDetail';
-import Footer from './components/Footer/Footer';
-import Checkout from './pages/Checkout/Checkout';
-import ContactUs from './pages/ContactUs/ContactUs';
-import MediaQueryContextProvider from './contexts/MediaQueryContextProvider';
-import Products from './pages/Products/Products';
-import CategoriesContextProvider from './contexts/CategoriesContextProvider';
-import ProductsContextProvider from './contexts/ProductsContextProvider';
-import CartItemContextProvider from './contexts/CartItemContextProvider';
+import { Box, CircularProgress, Typography } from '@mui/material';
+
+const Header = lazy(() => import('./components/Header/Header'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const About = lazy(() => import('./pages/About/About'));
+const ProductDetail = lazy(() => import('./pages/ProductDetail/ProductDetail'));
+const Footer = lazy(() => import('./components/Footer/Footer'));
+const Checkout = lazy(() => import('./pages/Checkout/Checkout'));
+const ContactUs = lazy(() => import('./pages/ContactUs/ContactUs'));
+
+const Products = lazy(() => import('./pages/Products/Products'));
+
+const ProductsContextProvider = lazy(() => import('./contexts/ProductsContextProvider'));
+const CartItemContextProvider = lazy(() => import('./contexts/CartItemContextProvider'));
+const MediaQueryContextProvider = lazy(
+	() => import('./contexts/MediaQueryContextProvider')
+);
+const CategoriesContextProvider = lazy(
+	() => import('./contexts/CategoriesContextProvider')
+);
 
 const App = () => {
 	return (
 		<Box
 			sx={{
-				// backgroundImage:
-				// 	'url(https://images.unsplash.com/photo-1515549832467-8783363e19b6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3027&q=80)',
-				// backgroundSize: 'cover',
-				// backgroundRepeat: 'no-repeat',
-				// backgroundPosition: 'center center',
+				backgroundImage:
+					'url(https://images.unsplash.com/photo-1515549832467-8783363e19b6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3027&q=80)',
+				backgroundSize: 'cover',
+				backgroundRepeat: 'no-repeat',
+				backgroundPosition: 'center center',
 				minHeight: '100vh',
 			}}>
-			<CartItemContextProvider>
-				<ProductsContextProvider>
-					<MediaQueryContextProvider>
-						<CategoriesContextProvider>
-							<Router>
-								<Header />
-								<Routes>
-									<Route path='/' element={<HomePage />} />
-									<Route path='/about' element={<About />} />
-									<Route path='/products' element={<Products />} />
-									<Route
-										path='product-detail/:id'
-										element={<ProductDetail />}
-									/>
-									<Route path='/checkout' element={<Checkout />} />
-									<Route path='/contact-us' element={<ContactUs />} />
-								</Routes>
-								<Footer />
-							</Router>
-						</CategoriesContextProvider>
-					</MediaQueryContextProvider>
-				</ProductsContextProvider>
-			</CartItemContextProvider>
+			<Suspense
+				fallback={
+					<Box
+						sx={{
+							display: 'flex',
+							flexDirection: 'column',
+							justifyContent: 'center',
+							alignItems: 'center',
+							height: '100vh',
+							backgroundColor: '#EFEFEF',
+						}}>
+						<CircularProgress color='secondary' size={75} />
+						<Typography
+							variant='h4'
+							sx={{ margin: '2.5rem', color: 'coral' }}>
+							Balci Store
+						</Typography>
+						<Typography variant='h6' sx={{ color: 'coral' }}>
+							Loading...
+						</Typography>
+					</Box>
+				}>
+				<CartItemContextProvider>
+					<ProductsContextProvider>
+						<MediaQueryContextProvider>
+							<CategoriesContextProvider>
+								<Router>
+									<Header />
+									<Routes>
+										<Route path='/' element={<HomePage />} />
+										<Route path='/about' element={<About />} />
+										<Route path='/products' element={<Products />} />
+										<Route
+											path='product-detail/:id'
+											element={<ProductDetail />}
+										/>
+										<Route path='/checkout' element={<Checkout />} />
+										<Route
+											path='/contact-us'
+											element={<ContactUs />}
+										/>
+									</Routes>
+									<Footer />
+								</Router>
+							</CategoriesContextProvider>
+						</MediaQueryContextProvider>
+					</ProductsContextProvider>
+				</CartItemContextProvider>
+			</Suspense>
 		</Box>
 	);
 };
